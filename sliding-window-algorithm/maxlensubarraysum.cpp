b54e00fe4ebd2ddba27565dd2ba86b256874a536
Ashwin jain <60666707+ashwinjain10320@users.noreply.github.com>
2021-10-01 20:38:44 +0530

    Add files via upload

---
+++ sliding-window-algorithm/maxlensubarraysum.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,sum;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++) cin>>a[i];
+    cin>>sum;
+    int i=0,j=1,currsum=a[i];
+    int maxsize=0;
+    while(j<n)
+    {
+        if(currsum<sum)
+        {
+            currsum+=a[j++];
+        }
+        else
+        {
+            if(currsum==sum) maxsize=max(maxsize,j-i);
+            currsum-=a[i++];
+        }
+    }
+    cout<<maxsize<<endl;
+}
